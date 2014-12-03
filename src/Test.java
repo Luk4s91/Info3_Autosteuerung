73006a386fd16c1ecb2082f41ed7860b82441901
Luk4s91 <luk4s.b@googlemail.com>
2014-12-03 14:09:11 +0100

    Neu gemacht
    03_12_2014
    Lukas

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+//import java.io.FileInputStream;
+//import java.io.InputStream;
+
+public class Test {
+
+	 BufferedReader bR= new BufferedReader(
+	    new InputStreamReader( System.in )
+     );
+	
+	public static void main(String[] args) {
+	//	System.out.println(System.in);
+		 
+	}
+
+}
