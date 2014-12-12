5ae08e25dabff5031e563f475d76a3c2e54f8d84
jd1990 <drehmaschine@googlemail.com>
2014-12-12 11:06:04 +0100

    Aufgabe 1 von Aufgabenblatt 2 zugefügt
    Class Prototyp erstellt und nach Aufgabe Attribute ect zugefügt.
    Bitte überprüfen, nachdem ich bei Felix nachgefragt habe, verstand ich
    "createInstance()" wie im Programm geschrieben und Kommentiert!

---
+++ src/Prototyp.java
@@ -0,0 +1,28 @@
+
+public class Prototyp {
+	
+	private String nameCommand;
+	
+	public void setNameCommand(String nameCommand) {
+		this.nameCommand = nameCommand;
+	}
+	public String getNameCommand() {
+		return nameCommand;
+	}
+	
+	public Command createInstance() //Soll über den Namen gucken, welcher Command es ist und davon ein neues Objekt erzeugen!? Prüfen obs so geht!!!
+	{
+		if (this.nameCommand=="Gear")
+		{
+			Gear g = new Gear(0, 0, 0, 0, 0);
+			return g;
+		}
+		else if (this.nameCommand=="Direction")
+		{
+			Direction d = new Direction(0, 0, 0, 0);
+			return d;
+		}
+		return null; //Gibt nur zwei Kommandos, oder??
+	}
+
+}
