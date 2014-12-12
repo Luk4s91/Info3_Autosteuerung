ba5c450da1d09dd185d06b3da9c540eec56c417f
jd1990 <drehmaschine@googlemail.com>
2014-12-12 11:42:19 +0100

    Aufgabe 2 von Aufgabenblatt 2 zugefügt
    Nach Aufgabenstellung die Methoden zugefügt und versucht das
    "Singleton-Pattern" anzuwenden.
    Methoden bisher noch ohne Inhalt!
    Vector-Class so richtig angewendet??

---
+++ src/Zentralverwaltung.java
@@ -0,0 +1,30 @@
+
+public class Zentralverwaltung {
+	private static Vector prototypen = null;
+	private static Vector controllprocess = null;
+	
+	private Zentralverwaltung(){}
+	
+	public void createPrototypen()
+	{
+		//INHALT
+	}
+	public void addStep(Prototyp iwas)
+	{
+	//INHALT	
+	}
+	public void removeStep(int iD)
+	{
+	//INHALT
+	}
+	public void incOrder(int iD)
+	{
+	//INHALT
+	}
+	public void decOrder(int iD)
+	{
+	//INHALT
+	}
+	//f)
+
+}
